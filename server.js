e53553e8d56cc63ca8a14c47d18e9c9008a53a84
Liam <liam@simplifybiz.com>
2025-05-21 12:59:55 -0600

    week3 learning activities

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require('./controllers/baseController')
+const inventoryRoute = require('./routes/inventoryRoute')
 
 /* ***********************
  * View Engine and Templates
@@ -26,11 +27,9 @@ app.use(static)
 
 // Index route
 app.get('/', baseController.buildHome)
-// app.get("/", function(req, res) {
-//   res.render("index", {
-//     title: "Home"
-//   })
-// })
+
+// Inventory route
+app.use('/inv', inventoryRoute)
 
 /* ***********************
  * Local Server Information
